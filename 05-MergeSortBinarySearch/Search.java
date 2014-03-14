4fd30ce18fa2b259134b806c5135efdc36d6c4ab
Adriel Mendoza <adrielmendoza97@gmail.com>
2014-03-14 00:53:30 -0400

    Uploaded HWS

--- 05-MergeSortBinarySearch/Search.java
+++ 05-MergeSortBinarySearch/Search.java
@@ -1,16 +1,40 @@
 public class Search{
-    /*public int rbsearch(int n, int[] L){
-	int mid = (L.length() - 1)/2;
-	if(L.length == 1 && n != L[0]){
-	    return -1;
+    public int rbsearch(int[] L, int n, int low, int high){
+	int mid = (low + high)/2;
+	if(L[mid] < n){
+	    return rbsearch(L, n, mid + 1, high);
 	}else if(L[mid] == n){
 	    return mid;
 	}else if(L[mid] > n){
-	    int[] new = new int[L.length - mid];
-	    for(int i = 0; i < L.length; i++){
-		new[i] = L.length
-	    return rbsearch(n, 
-    */
-    //not exactly sure how to break the array into a half excluding the middle value
+	    return rbsearch(L, n, low, mid - 1);
+	}else{
+	    return -1;
+	}
+    }
+
+    public int ibsearch(int[] L, int n, int low, int high){
+	int mid = (low + high)/2;
+	while((L[mid] != n) && (low <= high)){
+	    if(L[mid] > n){
+		low = mid + 1;
+	    }else{
+		high = mid - 1;
+	    }
+	}
+	if(low == high){
+	    return mid;
+	}else{
+	    return -1;
+	}
+    }
+
+    public static void main(String[] args){
+	Search s = new Search();
+	int[] i = {1, 3, 5, 6, 7, 8, 10, 12, 13, 15, 17, 18, 21, 23, 27};
+	System.out.println("Should return 4");
+	System.out.println(s.rbsearch(i, 7, 0, 14));
+	System.out.println("Should return 9");
+	System.out.println(s.rbsearch(i, 15, 0, 14));
+    }			 
 }
 	
--- 07-QuickSort/QuickSort.java
+++ 07-QuickSort/QuickSort.java
@@ -1,14 +1,17 @@
-/*import java.util.*;
+import java.util.*;
+import java.util.Arrays;
 
 public class QuickSort{
-    public void QuickSort(int[] a, int low, int high){
+    public int[] QuickSort(int[] a){
+	int low = 0;
+	int high = a.length - 1;
 	if(a.length <= 1){
 	    return a;
 	}
-	int i = (int)(Math.random() * (low - high)) + low;
+	int i = (int)((Math.random() * (high - low)) + low);
 	int pivot = a[i];
-	ArrayList<String> left;
-	ArrayList<String> right;
+	ArrayList<Integer> left = new ArrayList<Integer>();
+	ArrayList<Integer> right = new ArrayList<Integer>();
 	for(int j = 0; j < a.length; j++){
 	    if(pivot > a[j]){
 		left.add(a[j]);
@@ -17,7 +20,35 @@ public class QuickSort{
 		right.add(a[j]);
 	    }
 	}
+	int[] leftA = new int[left.size()];	
+	int[] rightA = new int[right.size()];
+	for(int x = 0; x < left.size(); x++)
+	    leftA[x] = left.get(x);
+	for(int x = 0; x < right.size(); x++)
+	    rightA[x] = right.get(x);
+	leftA = QuickSort(leftA);
+	rightA = QuickSort(rightA);
+	ArrayList<Integer> hold = new ArrayList<Integer>();
+	for(int j = 0; j < leftA.length; j++){
+	    hold.add(leftA[j]);
+	}
+	hold.add(pivot);
+	for(int j = 0; j < rightA.length; j++){
+	    hold.add(rightA[j]);
+	}				   
+	int[] resp = new int[a.length];
+	for(int j = 0; j < a.length; j++){
+	    resp[j] = hold.get(j);
+	}
+	return resp;    
     }
-}*/
+
+    public static void main(String[] args){
+	QuickSort q = new QuickSort();
+	int[] a = {9, 27, 15, 1, 12, 13, 17, 54, 162, 15425, 66, 3};
+	System.out.println(Arrays.toString(a));
+	System.out.println(Arrays.toString(q.QuickSort(a)));
+    }
+}
 	
 	
